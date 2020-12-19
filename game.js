62a9ce7a02a7ae1c009fd1b8e5f054e3277b939f
Kristen Groller <groller@rogers.com>
2020-12-19 13:39:26 -0500

    inputs validated

--- game.js
+++ game.js
@@ -1,19 +1,30 @@
  
 // Assignment code here
+
 //click the button to generate a password
  
 // what is the lenth of the password (btween 8 and 128)
- 
+function generatePassword(passwordLength) {
+    
+}
  
 // choose character type
- 
+var specialCharacters = "";
+var numCharacters = "0123456789";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+randPasswordArray[0] = specialCharacters;
+randPasswordArray[1] = numCharacters;
+randPasswordArray[3] = upperCase;
+randPasswordArray[4] = lowerCase;
+
 //validate input reponses
  
-var character = ("");
+
  
  
 for (var i = 0; i < 10; i++) {
-var num = (Math.floor(Math.random() * 129));
+var numCharacters = (Math.floor(Math.random() * 10));
 console.log(num);
 }
 // Get references to the #generate element
