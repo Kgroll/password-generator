b5b4fffe58cd73949f0b4a44dfdda0773d7e8748
Kristen Groller <groller@rogers.com>
2020-12-20 15:20:43 -0500

    passwords are being generated

--- game.js
+++ game.js
@@ -3,132 +3,100 @@
 var generateBtn = document.querySelector("#generate"); 
 var specialCharacter = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
 var numCharacters = "0123456789";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";   
 var passwordLength;
-var specialCheck;
-var numCheck;
-var upperCaseCheck;
-var lowerCaseCheck;
+var userPassword = "";
+var passwordGroup = "";
 
 //Determine the length of the password
 
-function determineLength() {
- passwordLength = window.prompt("How long would you like the password to be? Please enter a number from 8 to 128."); 
+
+ passwordLength = prompt("How long would you like the password to be? Please enter a number from 8 to 128."); 
 
     if (passwordLength<8){              
         alert("Please enter a number between 8 and 128");
         determineLength();
-    }else if (password>128){
+    }else if (passwordLength>128){
         alert("Please enter a number between 8 and 128");
         determineLength();
     }else if (isNaN(passwordLength)){
         alert("Please enter a number between 8 and 128");
-    }else
-        alert("Thank you! Please answer the following questions!");
+    }else{
+        alert("Thank you! Please answer the following questions, you must say yes to at least 1 type of character!");
 }
-    var specialCharacter = confirm("Do you want to use special characters? Yes or No");
-    if (specialCharacter) {
-     alert("You said yes to special characters!");
-    }
-    else {
-    alert("You said no to special characters!");
-}   
+    console.log(passwordLength);
 
-var numCharacters = confirm("Do you want to use numeric characters? Yes or No");
-  if (numCharacters) {
+// choose character type and validate input reponses
+      
+var selectspecialCharacter = confirm("Do you want to use special characters? Yes or No");
+ if (selectspecialCharacter) {
+     alert("You said yes to special characters!");    
+ }
+ else {
+     alert("You said no to special characters!");
+     
+   }
+   
+
+var selectnumCharacters = confirm("Do you want to use numeric characters? Yes or No");
+  if (selectnumCharacters) {
       alert("You said yes to numeric characters!");
+      
   }
   else {
     alert("You said no to numeric characters!")
   }
  
-var upperCase = confirm("Do you want to use uppercase characters? Yes or No");
-  if (upperCase) {
+var selectupperCaseCharacters = confirm("Do you want to use uppercase characters? Yes or No");
+  if (selectupperCaseCharacters) {
       alert("You said yes to uppercase characters!");
+      
   }
   else {
     alert("You said no to uppercase characters!")
   }
   
-var lowerCase = confirm("Do you want to use lowercase characters? Yes or No");
-  if (lowerCase) {
-      alert("You said yes to lower \case characters!");
+var selectlowerCaseCharacters = confirm("Do you want to use lowercase characters? Yes or No");
+  if (selectlowerCaseCharacters) {
+      alert("You said yes to lowercase characters!");
+      
   }
-  else {
+  else  {
     alert("You said no to lowercase characters!")
   }
-  console.log(specialCharacter, numCharacters, lowerCase, upperCase);
 
 
-
-
-
-
-
-
-
-
-
-
+  console.log(selectspecialCharacter, selectnumCharacters, selectlowerCaseCharacters, selectupperCaseCharacters);
 
 
 //click the button to generate a password
- 
-// what is the lenth of the password (btween 8 and 128)
-const randomFunc = {
-    special: getspecialCharacters,
-    number: getnumCharacters,
-    upper: getupperCase,
-    lower: getlowerCase
-};      
 
-
-
-
-
-    
-
-{
-// choose character type
-
-
-
-function getspecialCharacters() {
-    const specialCharacters = ["!@#$%^&*()_?"];
-    return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-}
-
-function getnumCharacters() {
-    return String.fromCharCode[Math.floor(Math.random() * 10) + 48];
-}
-
-function getupperCase() {
-    return String.fromCharCode[Math.floor(Math.random() * 26) + 65];
-}
-
-function getlowerCase() {
-    return String.fromCharCode[Math.floor(Math.random() * 26) + 97];
-}
-
-
-//validate input reponses
 function generatePassword() {
-    const len = length.value;
-    let password ="";
-for (let i = 0; i < len; i++) {
-    const x = generateX();
-    password += x;
-}
-
-}
-
-
- 
+    if (selectlowerCaseCharacters) {
+    passwordGroup += lowerCaseCharacters;
+    }
+    if (selectupperCaseCharacters) {
+    passwordGroup += upperCaseCharacters;
+    }
+    if (selectnumCharacters) {
+    passwordGroup += numCharacters;
+    }
+    if (selectspecialCharacter) {
+    passwordGroup += specialCharacter;
+    }
+    for (let i = 0; i < passwordLength; i++) {
+      userPassword += passwordGroup.charAt(
+      Math.floor(Math.random() * passwordGroup.length)
+      );
+    }
+    return userPassword;
+  }
+    
  
 
 // Get references to the #generate elements
-var generateBtn = document.querySelector("#generate");
+
  
 // Write password to the #password input
 function writePassword() {
@@ -141,4 +109,3 @@ function writePassword() {
  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-}
